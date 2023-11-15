caffe55401a6dabb44d0d73764a5eb02ad46bfbc
YaroslavKyba <68155188+YaroslavKyba@users.noreply.github.com>
2023-11-15 02:49:16 +0000

    Deploying to gh-pages from @ YaroslavKyba/goit-js-hw-09@5d5929821843f49ff2dbbb3609ae5974a419b69b 🚀

---
+++ 01-color-switcher.21017571.js
@@ -0,0 +1,2 @@
+!function(){var t=null,e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),o=document.querySelector("body");e.addEventListener("click",(function(){e.setAttribute("disabled",!0),t=setInterval((function(){o.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),n.addEventListener("click",(function(){clearInterval(t),e.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.21017571.js.map
---
+++ 01-color-switcher.44a3a194.js
@@ -0,0 +1,2 @@
+let t=null;const e=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]"),r=document.querySelector("body");e.addEventListener("click",(function(){e.setAttribute("disabled",!0),t=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),o.addEventListener("click",(function(){clearInterval(t),e.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.44a3a194.js.map
